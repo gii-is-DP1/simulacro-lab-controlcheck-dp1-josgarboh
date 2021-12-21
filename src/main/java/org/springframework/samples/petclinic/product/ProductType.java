763bd7d76cb0c88983457411063866eb1c5936b7
José Ignacio <garciaji700@gmail.com>
2021-12-21 21:04:58 +0100

    josgarboh

--- src/main/java/org/springframework/samples/petclinic/product/ProductType.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductType.java
@@ -1,11 +1,25 @@
 package org.springframework.samples.petclinic.product;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.samples.petclinic.model.BaseEntity;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class ProductType {
-    Integer id;
-    String name;
+@Entity
+@Table(name = "productypes")
+public class ProductType extends BaseEntity{
+    
+    @NotNull
+	@Column(name = "name", unique = true)
+	@Size(min = 3, max = 150)
+	String name;
+   
 }
